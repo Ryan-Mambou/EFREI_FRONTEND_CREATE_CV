fe05f712d76b084e9f3b5c51bd61132b350bae84
Ryan Djemtchemeu <ryanmambou@gmail.com>
2024-10-14 12:55:20 +0200

    feat: login and signup pages

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function Login() {
-  return <div>Login</div>;
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .max(10, "Must be 10 characters or less")
+        .required("Required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <div>
+      <h2>Enter your details to create your account</h2>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error-message">{formik.errors.email}</div>
+          ) : null}
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div className="error-message">{formik.errors.password}</div>
+          ) : null}
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .max(10, "Must be 10 characters or less")
+        .required("Required"),
+      confirmPassword: Yup.string()
+        .required("Required")
+        .oneOf([Yup.ref("password"), null], "Passwords must match"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <div>
+      <h1>Welcome to our CV APP!</h1>
+      <h2>Enter your details to create your account</h2>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="error-message">{formik.errors.email}</div>
+          ) : null}
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div className="error-message">{formik.errors.password}</div>
+          ) : null}
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            onChange={formik.handleChange}
+            value={formik.values.confirmPassword}
+          />
+          {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+            <div className="error-message">{formik.errors.confirmPassword}</div>
+          ) : null}
+        </div>
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
