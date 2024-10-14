45ae3864b8a78c7832ec320ef677023fe9206d61
Ryan Djemtchemeu <ryanmambou@gmail.com>
2024-10-14 11:59:13 +0200

    feat: routing implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import SignUp from "./pages/signup";
+import Timeline from "./pages/timeline";
+import Error from "./pages/Error";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Login />}></Route>
+        <Route path="/signup" exact element={<SignUp />}></Route>
+        <Route path="/timeline" exact element={<Timeline />}></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
