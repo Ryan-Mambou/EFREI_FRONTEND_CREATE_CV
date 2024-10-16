de8c476c945c4447145e36180d8b579e86c250d6
Ryan Djemtchemeu <ryanmambou@gmail.com>
2024-10-16 17:58:52 +0200

    feat: ui for recommendation

--- src/components/cvComponent.jsx
+++ src/components/cvComponent.jsx
@@ -1,14 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import Recommendation from "./recommendation";
 
-function CVComponent({ firstname, lastname, description, id }) {
+function CVComponent({
+  firstname,
+  lastname,
+  description,
+  id,
+  recommendations,
+}) {
+  const [recommendation, setRecommendation] = useState("");
+
+  const handleChange = (event) => {
+    setRecommendation(event.target.value);
+  };
+
+  const inputStyle = {
+    width: "100%",
+    padding: "0.5rem",
+    borderRadius: "0.5rem",
+    outline: "none",
+    marginTop: "1rem",
+  };
+
   return (
-    <div className="cv-item">
-      <h2>{`${firstname} ${lastname}`}</h2>
-      <p>{description}</p>
-      <a href={`/resume/${id}`} target="_blank" rel="noopener noreferrer">
-        View details in PDF
-      </a>
-    </div>
+    <>
+      <div className="cv-item">
+        <h2>{`${firstname} ${lastname}`}</h2>
+        <p>{description}</p>
+        <a href={`/resume/${id}`} target="_blank" rel="noopener noreferrer">
+          View details in PDF
+        </a>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "0.5rem",
+            marginTop: "1rem",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Enter a recommendation"
+            value={recommendation}
+            onChange={handleChange}
+          />
+          <button>Submit</button>
+        </div>
+      </div>
+      {/* {recommendations.length > 0 ? (
+        <div>
+          <h2>Recommendations</h2>
+          {recommendations?.map((recommendation) => (
+            <Recommendation
+              firstname={recommendation.firstname}
+              lastname={recommendation.lastname}
+              text={recommendation.text}
+            />
+          ))}
+        </div>
+      ) : null} */}
+    </>
   );
 }
 
--- src/components/recommendation.jsx
+++ src/components/recommendation.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-function Recommendation() {
-  return <div>Recommendation</div>;
+function Recommendation({ firstname, lastname, text }) {
+  const profileStyle = {
+    backgroundColor: "gray",
+    borderRadius: "50%",
+    height: "75px",
+    width: "75px",
+  };
+  return (
+    <div className="recommendation">
+      <div style={profileStyle}></div>
+      <div>
+        <h4>{`${firstname} ${lastname}`}</h4>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Recommendation;
