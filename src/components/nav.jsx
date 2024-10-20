7770bf55254a7a4984c0c71edee33975962d03cc
Zineddine-Harrar <Zineddine-Harrar@gmail.com>
2024-10-20 12:32:35 +0200

    add logout

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { HiHome } from "react-icons/hi2";
 import { IoSettings } from "react-icons/io5";
+import LogoutButton from "./Logout";
 
 function Nav() {
   const location = useLocation();
@@ -23,6 +24,10 @@ function Nav() {
         Build Resume
         <IoSettings className="icon" />
       </Link>
+
+      <div className={`link ${pathname === "/buildResume" ? "active" : ""}`}>
+        <LogoutButton />
+      </div>
     </nav>
   );
 }
